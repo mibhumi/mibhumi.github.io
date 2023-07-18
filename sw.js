af0d85748d133b93886fed071698e78aeef496c0
Bhumi Patel <bhumipatel1196@gmail.com>
2023-07-17 20:54:34 -0400

    update experience

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-88829dc477ef76f6191f.js"
+    "url": "webpack-runtime-6027287af068bd856ff8.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2779bd0b44aea187cfe168025f690fea"
+    "revision": "afd764463f19078e55c2fdebb7678111"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
