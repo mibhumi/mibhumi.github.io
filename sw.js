6db4f20d4597684f603ae57d6da62a1691f4865d
mibhumi <bhumipatel1196@gmail.com>
2026-02-05 09:16:41 -0500

    fix format

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-53b8da7efe12674a21b0.js"
+    "url": "webpack-runtime-4cba9a0a220521e79257.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d2ed6df9a3a4e4d2c8542d09f8b192e3"
+    "revision": "726a787b74d0fdeeb8f0cc78d47db3a0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
